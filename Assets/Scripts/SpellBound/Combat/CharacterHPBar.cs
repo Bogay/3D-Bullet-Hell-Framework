07dc0d8c6e07d1d0d0bfed60e7f8683d3a6d4764
bogay <pojay11523@gmail.com>
2023-12-24 17:39:05 +0800

    feat: HP bar for boss
    dirty. but at least it works...

---
+++ Assets/Scripts/SpellBound/Combat/CharacterHPBar.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using Cysharp.Threading.Tasks.Linq;
+using SpellBound.Core;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SpellBound.Combat
+{
+    public class CharacterHPBar : MonoBehaviour
+    {
+        [SerializeField]
+        private Character character;
+        [SerializeField]
+        private Slider hpSlider;
+
+        private void Start()
+        {
+            var ct = this.GetCancellationTokenOnDestroy();
+            this.hpSlider.enabled = false;
+            this.startAsync(ct).Forget();
+        }
+
+        private async UniTask startAsync(CancellationToken ct)
+        {
+            BossEnemyController bossEnemyController = null;
+            while (!ct.IsCancellationRequested)
+            {
+                bossEnemyController = FindObjectOfType<BossEnemyController>();
+                if (bossEnemyController != null)
+                {
+                    break;
+                }
+                await UniTask.WaitForSeconds(0.5f, cancellationToken: ct);
+            }
+
+            this.character = bossEnemyController.character;
+            this.hpSlider.enabled = true;
+            var cts = CancellationTokenSource.CreateLinkedTokenSource(ct, bossEnemyController.GetCancellationTokenOnDestroy());
+            await foreach (var _ in UniTaskAsyncEnumerable.EveryUpdate().WithCancellation(cts.Token))
+            {
+                this.hpSlider.maxValue = this.character.MaxHP.Value();
+                this.hpSlider.value = this.character.HP;
+            }
+        }
+    }
+}
